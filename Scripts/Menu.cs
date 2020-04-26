f7df58b2a653af87c970749c3fd14cbf0f19d9b3
jantepya <jantepya@sfu.ca>
2020-04-26 11:30:31 -0700

    renamed files, added namespace, and updated GUI

---
+++ Scripts/Menu.cs
@@ -0,0 +1,97 @@
+﻿using UnityEditor;
+using UnityEngine;
+using Stopwatch = System.Diagnostics.Stopwatch;
+
+namespace Voxelizer
+{
+    public class Menu : EditorWindow
+    {
+        [MenuItem("Window/Voxelize Sprite")]
+
+        public static void ShowWindow()
+        {
+            EditorWindow.GetWindow(typeof(Voxelizer.Menu));
+        }
+
+        private Sprite _sprite;
+        private Material _material;
+        private Shader _shader;
+        private bool _useMeshOptimizer;
+
+        void OnGUI()
+        {
+            _sprite = (Sprite)EditorGUILayout.ObjectField("Selected Sprite", _sprite, typeof(Sprite), true);
+
+            string debugText = null;
+
+            if (_sprite == null)
+            {
+                GUI.enabled = false;
+                debugText = "Need to Select a Sprite";
+            }
+            else
+            {
+                if (_sprite.texture.isReadable == false)
+                {
+                    GUI.enabled = false;
+                    debugText = "Read/Write needs to be enabled in the sprite's Import Settings";
+                }
+                else if (_sprite.texture.format != TextureFormat.RGBA32)
+                {   
+                    debugText = "For best results, set sprite compression format to RGBA32 before converting";
+                }
+            }
+
+            _material = (Material)EditorGUILayout.ObjectField("Selected Sprite", _material, typeof(Material), true);
+            EditorGUILayout.Space();
+
+
+            GUILayout.BeginVertical("HelpBox");
+            _useMeshOptimizer = EditorGUILayout.Toggle("Use Mesh Optimizer", _useMeshOptimizer);
+            EditorGUILayout.HelpBox("Unity's mesh optimizer optimezes the Mesh data to improve rendering performance. This operation can take a few seconds or more for complex meshes", MessageType.Info);
+            GUILayout.EndVertical();
+            EditorGUILayout.Space();
+
+
+            if (GUILayout.Button("Create"))
+            {
+                VoxelizeSprite(_sprite, _material, _useMeshOptimizer);
+            }
+            GUI.enabled = true;
+
+            if (debugText != null)
+            {
+                EditorGUILayout.HelpBox(debugText, MessageType.Warning);
+            }
+        }
+
+        /*
+        * Create a 3D voxel GameObject from a texture
+        */
+        public static void VoxelizeSprite(Sprite sprite, Material material, bool useMeshOptimizer = false)
+        {
+            var timer = Stopwatch.StartNew();
+
+            Mesh mesh = Voxelizer.Util.VoxelizeTexture2D(sprite.texture);
+
+            if (useMeshOptimizer)
+            {
+                MeshUtility.Optimize(mesh);
+            }
+
+            timer.Stop();
+
+            string meshName = sprite.name + "_3D";
+
+            Debug.Log($"[VoxelizeSprite] {meshName}: Mesh created after {timer.ElapsedMilliseconds} milliseconds");
+
+            var sprite3D = new GameObject(sprite.name + "_3D");
+            
+            var meshFilter = sprite3D.AddComponent<MeshFilter>();
+            meshFilter.sharedMesh = mesh;
+
+            var meshRenderer = sprite3D.AddComponent<MeshRenderer>();
+            meshRenderer.material = material == null ? Resources.Load<Material>("Materials/3DSpriteMaterial") : material;
+        } 
+    }
+}
---
+++ Scripts/Util.cs
@@ -0,0 +1,180 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Voxelizer
+{
+    public static class Util
+    {
+        /*
+        * Create a Mesh object from a Texture2D object
+        */
+        public static Mesh VoxelizeTexture2D(Texture2D texture)
+        {
+            texture.Apply(updateMipmaps: true, makeNoLongerReadable: false);
+            texture.filterMode = FilterMode.Point;
+
+            if (texture.format != TextureFormat.RGBA32)
+            {
+                Debug.LogWarning("For best results, set sprite format to RGBA32 from Import Settings");
+            }
+
+            int height = texture.height;
+            int width = texture.width;
+            Color32[] colorBuffer = texture.GetPixels32();
+
+            var mesh = new Mesh();
+
+            var vertices = GenerateVertices(height, width);
+            mesh.SetVertices(vertices);
+
+            var normals = GenerateNormals(height, width);
+            mesh.SetNormals(normals);
+
+            var vertexColors = GenerateColors(colorBuffer, height, width);
+            mesh.SetColors(vertexColors);
+
+            var triangles = GenerateTriangles(colorBuffer, width);
+            mesh.SetTriangles(triangles, 0);
+
+            return mesh;
+        }
+
+
+        /*
+        * Generate 24 vertices cube for every pixel in the texture
+        */
+        private static List<Vector3> GenerateVertices(int height, int width)
+        {
+            List<Vector3> vertices = new List<Vector3>(24*(height * width));
+
+            float scale = 1f;
+
+            for (int i = height-1; i >= 0 ; i--)
+            {
+                float y = -i * scale;
+                for (int j = 0; j < width; j++)
+                {
+                    float x = j * scale;
+
+                    Vector3[] cube = new Vector3[8];
+
+                    // bottom
+                    cube[0] = new Vector3(x, y, scale);
+                    cube[1] = new Vector3(x + scale, y, scale);
+                    cube[2] = new Vector3(x + scale, y, -scale);
+                    cube[3] = new Vector3(x, y, -scale);
+
+                    // top
+                    cube[4] = new Vector3(x, y + scale, scale);
+                    cube[5] = new Vector3(x + scale, y + scale, scale);
+                    cube[6] = new Vector3(x + scale, y + scale, -scale);
+                    cube[7] = new Vector3(x, y + scale, -scale);
+
+                    vertices.AddRange(new List<Vector3>
+                    {
+                        cube[0], cube[1], cube[2], cube[3], // Bottom
+                        cube[7], cube[4], cube[0], cube[3], // Left
+                        cube[4], cube[5], cube[1], cube[0], // Front
+                        cube[6], cube[7], cube[3], cube[2], // Back
+                        cube[5], cube[6], cube[2], cube[1], // Right
+                        cube[7], cube[6], cube[5], cube[4]  // Top
+                    });  
+                }
+            }
+
+            return vertices;
+        }
+
+        private static int[] GenerateTriangles(IList<Color32> colorBuffer, int width)
+        {
+            // triangle values are indices of vertices array
+            List<int> triangles = new List<int>(colorBuffer.Count);
+
+            // colorbuffer pixels are laid out left to right, 
+            // bottom to top (i.e. row after row)
+            for (int i = 0; i < 24*colorBuffer.Count; i+=24)
+            {
+                if (colorBuffer[i/24].a != 0)
+                {
+                    triangles.AddRange(new int[]
+                    {
+                        // Bottom
+                        i + 3, i + 1, i,
+                        i + 3, i + 2, i + 1,
+
+                        // Left     	
+                        i + 7, i + 5, i + 4,
+                        i + 7, i + 6, i + 5,
+
+                        // Front
+                        i + 11, i + 9, i + 8,
+                        i + 11, i + 10, i + 9,
+
+                        // Back
+                        i + 15, i + 13, i + 12,
+                        i + 15, i + 14, i + 13,
+
+                        // Right
+                        i + 19, i + 17, i + 16,
+                        i + 19, i + 18, i + 17,
+
+                        // Top
+                        i + 23, i + 21, i + 20,
+                        i + 23, i + 22, i + 21,
+                    });
+                }
+            }
+            
+            return triangles.ToArray();
+        }
+
+        private static List<Vector3> GenerateNormals(int height, int width)
+        {
+            List<Vector3> normals = new List<Vector3>(24*height*width);
+
+            Vector3 up = Vector3.up;
+            Vector3 down = Vector3.down;
+            Vector3 forward = Vector3.forward;
+            Vector3 back = Vector3.back;
+            Vector3 left = Vector3.left;
+            Vector3 right = Vector3.right;
+
+            for (int i = height - 1; i >= 0; i--)
+            {
+                for (int j = 0; j < width; j++)
+                {
+                    normals.AddRange(new List<Vector3>
+                    {
+                        down, down, down, down,             // Bottom
+                        left, left, left, left,             // Left
+                        forward, forward, forward, forward,	// Front
+                        back, back, back, back,             // Back
+                        right, right, right, right,         // Right
+                        up, up, up, up	                    // Top
+                    });
+                }
+            }
+
+            return normals;
+        }
+
+        private static List<Color32> GenerateColors(IList<Color32> colorBuffer, int height, int width)
+        {
+            List<Color32> vertexColors = new List<Color32>(24 * height * width);
+            for (int i = 0; i < height; i++)
+            {
+                for (int j = 0; j < width; j++)
+                {
+                    Color32 c = colorBuffer[j + i * width];
+                    for (int k = 0; k < 24; k++)
+                    {
+                        vertexColors.Add(c);
+                    }
+                }
+            }
+
+            return vertexColors;
+        }
+
+    }
+}
